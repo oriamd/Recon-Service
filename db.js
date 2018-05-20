9a6b4bfc8c6a0e5a0509880a83a04e11a8c6e782
oriamd <ori.amuday@gmail.com>
2018-05-20 14:22:42 +0300

    global var for heroku

--- db.js
+++ db.js
@@ -2,11 +2,17 @@ const mysql = require('mysql');
 const config = require('./config');
 const dbLogger = require('Logger')('db');
 
-const con = mysql.createConnection(config.db);
+const con = mysql.createConnection({
+        host: process.env.DB_HOST || config.db.host,
+        user: process.env.DB_USERNAME || config.db.username,
+        password: process.env.DB_PASSWORD || config.db.password,
+        database: "recon"
+    }
+);
 con.connect(function (err) {
     if (err) {
         dbLogger.writeLog(err);
-    }else {
+    } else {
         dbLogger.writeLog("db connected to " + config.db.host);
     }
 });
