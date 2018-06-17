5a7a6915dc382a8d7071c2bd1b68468223b3c677
oriamd <ori.amuday@gmail.com>
2018-06-17 18:54:27 +0300

    DB pool

--- app.js
+++ app.js
@@ -7,6 +7,21 @@ const TargetController = require('./controllers/targetController');
 const ReconUnitController = require('./controllers/reconunitController');
 const MessageController = require('./controllers/messageController');
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.use(function (req, res, next) {
     appLogger.writeLog(`New Request ${req.method} '${req.originalUrl}' from  ${req.ip}`);
